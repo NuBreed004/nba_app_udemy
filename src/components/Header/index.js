8947e522033ccf644b3f45e0a0679395a4c259d5
Alexander <nubreed004@gmail.com>
2018-08-19 14:26:30 +0300

    Images Folder Added to Public Images Render Function Created in Header Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import style from './header.css';
+import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <header>This is a Header</header>
-);
+const Header = () => {
+
+  const logo = () => {
+    return (
+      <Link to="/">
+        <img src="/images/nba_logo.png" alt="nba logo"/>
+      </Link>
+    )
+  }
+
+  return (
+      <header className={style.header}>
+        <div className={style.headerOpt}>
+          { logo() }
+        </div>
+      </header>
+  )
+};
 
 export default Header;
