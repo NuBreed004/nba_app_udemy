f0211d67b0562fa3e8f5b081ed5acaa651829e0f
Alexander <nubreed004@gmail.com>
2018-08-21 22:13:16 +0300

    New Branch on Laptop Created

--- src/components/widgets/NewsList/index.js
+++ src/components/widgets/NewsList/index.js
@@ -5,14 +5,25 @@ import {
 } from 'react-transition-group';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { URL } from '../../../helpers';
 
 class NewsList extends Component {
 
   state = {
+    news: []
+  }
 
+  componentWillMount() {
+    axios.get(`${URL}/articles`)
+          .then(response => {
+            this.setState({
+              news: [...this.state.news, ...response.data]
+            })
+          })
   }
 
   render() {
+    console.log(this.state.news)
     return (
       <div>Hello!</div>
     );
